98a778814807b1c0b2c9ab8e6e24bffca6c2bcba
kris0454 <44412830+kris0454@users.noreply.github.com>
2020-12-08 10:38:24 +0100

    Dodany test dla funkcji LogIn

---
+++ TestJednostkowyServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestJednostkowySerwer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestJednostkowySerwer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b9075c9-a844-4e41-a777-338a4beaacc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestJednostkowyServer/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System.Net;
+
+namespace TestJednostkowySerwer
+{
+    [TestClass]
+    public class ServerTest
+    {
+        [TestMethod]
+        public void TestMethodIfPalimdrom()
+        {
+            string slowotestowe = "kajak";
+            MyTCPServer test = new MyTCPServer(IPAddress.Parse("127.0.0.1"), 2048);
+            Assert.IsTrue(test.CheckIfPalindrome(slowotestowe));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+        }
+    }
+}
